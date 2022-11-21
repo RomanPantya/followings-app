94f6d11aa1ffca31859b7dd383db160409aa55a9
RomanPantya <romanpantya@gmail.com>
2022-11-21 17:55:04 +0200

    import Typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DbConnectionOptions } from './db-connection-options';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(DbConnectionOptions)],
   controllers: [AppController],
   providers: [AppService],
 })
