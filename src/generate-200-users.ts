ca7c028b2bb56401415be0d3c942364bad4aedb4
RomanPantya <romanpantya@gmail.com>
2022-11-21 23:08:23 +0200

    genereted ten users

--- src/generate-200-users.ts
+++ src/generate-200-users.ts
@@ -1,18 +1,21 @@
 import { faker } from '@faker-js/faker';
 import { UserEntity } from './entities/user.entity';
 
+const users = [];
+Array.from({ length: 10 }).forEach(() => {
+  users.push(generate200Users());
+});
 function generate200Users() {
- // const users = [];
   const user: Omit<UserEntity, 'id'> = new UserEntity();
   user.email = faker.internet.email();
-
-  console.log(user);
+  return user;
 }
+console.log(users);
 
-async function main() {
-  const users = generate200Users();
+// async function main() {
+//   const users = generate200Users();
 
-  console.log(users);
-}
+//   console.log(users);
+// }
 
-main();
+// main();
