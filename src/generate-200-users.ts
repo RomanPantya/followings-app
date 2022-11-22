ed432d2d3ad50c5aa9a4e17661ce74763f31df3c
RomanPantya <romanpantya@gmail.com>
2022-11-22 02:09:04 +0200

    completed genereted 200 users

--- src/generate-200-users.ts
+++ src/generate-200-users.ts
@@ -1,24 +1,28 @@
 import { faker } from '@faker-js/faker';
+import { DataSource } from 'typeorm';
+import { DbConnectionOptions } from './db-connection-options';
 import { UserEntity } from './entities/user.entity';
 
 const users = [];
-generete200users();
+
 function generete200users() {
   Array.from({ length: 10 }).forEach(() => {
     users.push(generateUser());
   });
 }
-console.log(users);
+
 function generateUser() {
   const user: Omit<UserEntity, 'id'> = new UserEntity();
   user.email = faker.internet.email();
   return user;
 }
 
-// async function main() {
-//   const users = generate200Users();
+async function main() {
+  const dataSource = new DataSource(DbConnectionOptions);
+  await dataSource.initialize();
+  await generete200users();
+  const UserRepo = dataSource.getRepository(UserEntity);
+  await UserRepo.insert(users);
+}
 
-//   console.log(users);
-// }
-
-// main();
+main();
