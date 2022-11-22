67de12e04b4a5e08c2b7ddbf577f47744c02db77
RomanPantya <romanpantya@gmail.com>
2022-11-22 20:57:15 +0200

    UserService

--- src/app/users/user.module.ts
+++ src/app/users/user.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from 'src/entities/user.entity';
 import { UserController } from 'src/app/users/user.controller';
+import { UserService } from './user.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
+  providers: [UserService],
 })
 export class UserModule {}
