5900a244e2357071a6ed0e095f7bae25f03ebc9b
RomanPantya <romanpantya@gmail.com>
2022-11-22 20:58:02 +0200

    inject repository

--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private usersRepository: Repository<UserEntity>,
+  ) {}
+}
