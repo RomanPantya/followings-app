ff29f5cc05307efc97316e3513c6dd77c93fcce6
RomanPantya <romanpantya@gmail.com>
2022-11-27 17:06:17 +0200

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('Followings app API')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('doc', app, document);
 
   app.useGlobalPipes(new ValidationPipe({
     transformOptions: {
